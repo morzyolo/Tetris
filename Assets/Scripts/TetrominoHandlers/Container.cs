9b4a74a5ec64307d90bbf0f57bdca5686cf6ccc0
morz <morz21st@gmail.com>
2023-12-28 14:30:54 +0300

    Added tetromino container

---
+++ Assets/Scripts/TetrominoHandlers/Container.cs
@@ -0,0 +1,16 @@
+﻿using Tetrominoes;
+
+namespace TetrominoHandlers
+{
+	public class Container
+	{
+		public ITetromino CurrentTetromino => _currentTetramino;
+		private ITetromino _currentTetramino;
+
+		public Container(ITetromino startTetromino)
+			=> SwitchTetromino(startTetromino);
+
+		public void SwitchTetromino(ITetromino newTetromino)
+			=> _currentTetramino = newTetromino;
+	}
+}
