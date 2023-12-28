904d040ef7899e5668e3420201378dba7df94c29
morz <morz21st@gmail.com>
2023-12-28 15:30:42 +0300

    Added tetromino rotator

---
+++ Assets/Scripts/TetrominoHandlers/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using TetrominoGridHandlers;
+
+namespace TetrominoHandlers
+{
+	public class Rotator
+	{
+		private readonly TetrominoGrid _grid;
+		private readonly Container _tetramino;
+
+		public Rotator(TetrominoGrid grid, Container tetramino)
+		{
+			_grid = grid;
+			_tetramino = tetramino;
+		}
+
+		public void Rotate(float direction)
+		{
+			//TODO
+		}
+	}
+}
