f4b2382e391f019618ac6d07cb11131e6fc1fd7d
morz <morz21st@gmail.com>
2023-12-28 15:31:31 +0300

    Added tetromino switcher

---
+++ Assets/Scripts/TetrominoHandlers/Switcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TetrominoGridHandlers;
+
+namespace TetrominoHandlers
+{
+	public class Switcher : IDisposable
+	{
+		private readonly TetrominoFactory _factory;
+		private readonly DownMover _downMover;
+
+		public Switcher(TetrominoFactory factory, DownMover downMover)
+		{
+			_factory = factory;
+			_downMover = downMover;
+
+			_downMover.TetrominoPlaced += ChangeTetromino;
+		}
+
+		private void ChangeTetromino()
+		{
+			//TODO
+		}
+
+		public void Dispose()
+		{
+			_downMover.TetrominoPlaced -= ChangeTetromino;
+		}
+	}
+}
