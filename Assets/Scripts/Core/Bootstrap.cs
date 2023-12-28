7af62f7249290ec68fe76cff38e5bb4fb3fabd2b
morz <morz21st@gmail.com>
2023-12-28 15:35:39 +0300

    Added bootstrap

---
+++ Assets/Scripts/Core/Bootstrap.cs
@@ -0,0 +1,33 @@
+using InputHandlers;
+using TetrominoGridHandlers;
+using TetrominoHandlers;
+using UnityEngine;
+
+namespace Core
+{
+	public class Bootstrap : MonoBehaviour
+	{
+		[SerializeField] private TetrominoGrid _grid;
+		[SerializeField] private InputHandler _input;
+
+		private Switcher _switcher;
+
+		private void Awake()
+		{
+			TetrominoFactory tetrominoFactory = new();
+			Container container = new(tetrominoFactory.Produce());
+			HorizontalMover horizontalMover = new(_grid, container);
+			Rotator rotator = new(_grid, container);
+			Control control = new(horizontalMover, rotator);
+			_input.Init(control);
+
+			DownMover downMover = new(_grid, container);
+			_switcher = new(tetrominoFactory, downMover);
+		}
+
+		private void OnDisable()
+		{
+			_switcher.Dispose();
+		}
+	}
+}
