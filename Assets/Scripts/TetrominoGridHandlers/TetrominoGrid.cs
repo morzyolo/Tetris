92ea4930abbb2b8febc91c866bb9ea573b6388d2
morz <morz21st@gmail.com>
2023-12-29 17:15:06 +0300

    Added tetromino spawn

--- Assets/Scripts/TetrominoGridHandlers/TetrominoGrid.cs
+++ Assets/Scripts/TetrominoGridHandlers/TetrominoGrid.cs
@@ -1,3 +1,4 @@
+using Tetrominoes;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -10,10 +11,20 @@ namespace TetrominoGridHandlers
 
 		[Header("Grid stats")]
 		[SerializeField] private Vector2Int _gridSize = new(10, 18);
+		[SerializeField] private Vector2Int _spawnPosition = new(5, 16);
+
 
 		private void Awake()
 		{
 			_gridSetup.ResizeGrid(_gridSize);
+		}
+
+		public void SpawnTetromino(Tetromino tetromino)
+		{
+			for (int i = 0; i < tetromino.Cells.Length; i++)
+				_tilemap.SetTile(
+					(Vector3Int)(tetromino.Cells[i] + _spawnPosition),
+					tetromino.Tile);
 		}
 	}
 }
