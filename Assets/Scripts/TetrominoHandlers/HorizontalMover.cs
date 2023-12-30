942448200067c4c74d9de83ea3a9fd89dcdc31d4
morz <morz21st@gmail.com>
2023-12-30 17:38:13 +0300

    Added horizontal movement of tetrominoes

--- Assets/Scripts/TetrominoHandlers/HorizontalMover.cs
+++ Assets/Scripts/TetrominoHandlers/HorizontalMover.cs
@@ -1,21 +1,33 @@
 using TetrominoGridHandlers;
+using UnityEngine;
 
 namespace TetrominoHandlers
 {
 	public class HorizontalMover
 	{
 		private readonly TetrominoGrid _grid;
-		private readonly Container _tetramino;
+		private readonly Container _container;
 
 		public HorizontalMover(TetrominoGrid grid, Container tetramino)
 		{
 			_grid = grid;
-			_tetramino = tetramino;
+			_container = tetramino;
 		}
 
 		public void Move(float direction)
 		{
-			//TODO
+			_grid.ClearTetrominoTiles(_container.CurrentTetromino);
+
+			Vector2Int newPosition = _container.CurrentTetromino.Position;
+			if (direction > 0)
+				newPosition.x++;
+			else
+				newPosition.x--;
+
+			if (_grid.IsValidTetrominoPosition(newPosition, _container.CurrentTetromino))
+				_container.CurrentTetromino.Position = newPosition;
+
+			_grid.PlaceTetromino(_container.CurrentTetromino);
 		}
 	}
 }
