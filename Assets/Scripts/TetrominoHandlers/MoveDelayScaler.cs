0dd992fb23d31cf8a6251257d7bee1a5cb9bd8a8
morz <morz21st@gmail.com>
2024-01-12 16:55:13 +0300

    Added MoveDelayScaler

---
+++ Assets/Scripts/TetrominoHandlers/MoveDelayScaler.cs
@@ -0,0 +1,21 @@
+﻿namespace TetrominoHandlers
+{
+	public class MoveDelayScaler
+	{
+		private readonly float _defaultDelayScale = 1f;
+		private readonly float _acceleratedDelayScale = 0.2f;
+
+		private readonly PeriodicDownMover _mover;
+
+		public MoveDelayScaler(PeriodicDownMover mover)
+		{
+			_mover = mover;
+		}
+
+		public void SetDefaultDelay()
+			=> _mover.ScaleMoveDelay(_defaultDelayScale);
+
+		public void SetAcceleratedDelay()
+			=> _mover.ScaleMoveDelay(_acceleratedDelayScale);
+	}
+}
