6ec088f43ad259acc7f36a8ab4b503ccce7a7641
morz <morz21st@gmail.com>
2024-01-15 18:55:22 +0300

    Added end presenter

---
+++ Assets/Scripts/Presenters/EndPresenter.cs
@@ -0,0 +1,48 @@
+﻿using InputHandlers;
+using System;
+using TetrominoHandlers;
+using UnityEngine.SceneManagement;
+using Views;
+
+namespace Presenters
+{
+	public sealed class EndPresenter : IDisposable
+	{
+		private readonly EndView _view;
+		private readonly InputHandler _input;
+		private readonly Switcher _switcher;
+
+		public EndPresenter(
+			EndView view,
+			Switcher switcher,
+			InputHandler input
+		)
+		{
+			_view = view;
+			_input = input;
+			_switcher = switcher;
+
+			_view.Hide();
+			_switcher.OntSwitchFailed += Show;
+			_view.OnRestartButtonPressed += RestartGame;
+		}
+
+		public void Dispose()
+		{
+			_switcher.OntSwitchFailed -= Show;
+			_view.OnRestartButtonPressed -= RestartGame;
+		}
+
+		private void Show()
+		{
+			_input.Disable();
+			_view.Show();
+		}
+
+		private void RestartGame()
+		{
+			int sceneId = SceneManager.GetActiveScene().buildIndex;
+			SceneManager.LoadScene(sceneId);
+		}
+	}
+}
