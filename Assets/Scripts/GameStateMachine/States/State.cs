1fa464c1a5d08f0dede8306ccfe5d7f6a5ca80ca
morz <morz21st@gmail.com>
2024-01-17 16:44:35 +0300

    Added base state

---
+++ Assets/Scripts/GameStateMachine/States/State.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace GameStateMachine.States
+{
+	public abstract class State
+	{
+		public event Action OnEntered;
+		public event Action OnExited;
+
+		protected readonly StateMachine StateMachine;
+
+		protected State(StateMachine stateMachine)
+		{
+			StateMachine = stateMachine;
+		}
+
+		public void Enter()
+		{
+			OnEntered?.Invoke();
+		}
+
+		public void Exit()
+		{
+			OnExited.Invoke();
+		}
+
+		public abstract void GoToNext();
+	}
+}
